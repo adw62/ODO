421761a6fc5cf2266a47be95b10e612337b137aa
adw62 <38112687+adw62@users.noreply.github.com>
2020-01-13 15:19:34 +0000

    Add files via upload

--- ODO/ODO.py
+++ ODO/ODO.py
@@ -21,6 +21,8 @@ from generator.data_structs import Dataset_gen
 from generator.utils import seq_to_smiles, get_moments
 from generator.train_prior import pretrain
 
+from scipy import spatial
+
 rdBase.DisableLog('rdApp.error')
 
 class ODO(object):
@@ -30,10 +32,11 @@ class ODO(object):
         self.gen_data_dir = './generator/data/'
         self.gen_ckpt_file = self.gen_data_dir+'vector_based/Prior.ckpt'
 
-        self.target = 7.5
+        self.target = 8.0
         #Make a discriminative model and use finite differnces to solev this modle for a set of inputs predicted to give a set target
-        self.y_property, self.x_solution = ODO.discrim(self, target_property=self.target)
-        print('Optimized solution has activity {}'.format(self.y_property))
+        self.y_property, self.x_solution = ODO.discrim(self, target_property=self.target, train=False)
+
+        print('Target activity {}, optimized solution achieved activity {}'.format(self.target, self.y_property[0]))
 
         #Convert the solution to the decriminative modle to a form that can be fed into a genereative model
         ODO.discrim_to_gen(self)
@@ -62,13 +65,19 @@ class ODO(object):
             raise ValueError('Try deleting {}'.format(self.discrim_data_dir+'output_vecs.csv'))
 
         #test if generating smiles close to generation vectors
-        ODO.test_vector_msd(self, generated_vecs)
+        #ODO.test_vector_msd(self, generated_vecs)
 
         predict = ODO.predict_with_discrim(self, generated_vecs)
-        print(predict)
-        print(np.average(predict), np.std(predict))
-        thresh = [True if x >= 8.5 else False for x in predict]
-        print('{}/{}'.format(thresh.count(True), len(thresh)))
+        np.savetxt('./{}.dat'.format(self.target), predict)
+        print('Average activity {} and std {}'.format(np.average(predict), np.std(predict)))
+        thresh_hold = 7.0
+        thresh = [True if x >= thresh_hold else False for x in predict]
+        smi_above_thresh = [[x, y] for x, y, z in zip(generated_smis, predict, thresh) if z is True]
+        print('Number of compunds created = {}'.format(len(thresh)))
+        print('Precent of compounds with activity above {} = {}'.format(thresh_hold,
+                                                                        100*(thresh.count(True)/len(thresh))))
+        for x in smi_above_thresh:
+            print(x[0])
 
     def test_vector_msd(self, generated_vectors):
         vectors = pd.read_csv(self.discrim_data_dir + 'rounded.csv', header=0)
@@ -96,8 +105,13 @@ class ODO(object):
         net.to('cuda')
         return net
 
-    def discrim(self, target_property, optimize=True):
-        net = ODO.load_discrim(self)
+    def discrim(self, target_property, train=True, optimize=True):
+        if train:
+            net = ODO.load_discrim(self)
+        else:
+            file = './net.ckpt'
+            print('Loading discriminator from file {}'.format(file))
+            net = ODO.load_discrim(self, file, True)
 
         if not os.path.exists(self.discrim_data_dir+'input_mols.csv'):
             raise ValueError('Please add mols to {}'.format(self.discrim_data_dir+'input_mols.csv'))
@@ -110,33 +124,73 @@ class ODO(object):
             print('Calculating vectors')
             mols = pd.read_csv(self.discrim_data_dir+'input_mols.csv', header=0)
             mols = [x[0] for x in mols.values]
-            get_latent_vecs(mols, self.discrim_data_dir, 'input_train.csv')
+            target = pd.read_csv(self.discrim_data_dir+'input_target.csv', header=0)
+            target = [x[0] for x in target.values]
+            get_latent_vecs(mols, self.discrim_data_dir, 'input_train.csv', target)
 
 
         data = Dataset_discrim(self.discrim_data_dir+'input_train.csv', self.discrim_data_dir+'input_target.csv')
         frac_split = 0.7
-        split = [math.floor(len(data)*frac_split), math.ceil(len(data)*(1-frac_split))]
+        train_len = math.floor(len(data)*frac_split)
+        split = [train_len, len(data)-train_len]
         training_data, testing_data = torch.utils.data.random_split(data, split)
         loader = Data.DataLoader(training_data, batch_size=250, shuffle=True,
                                  drop_last=True, collate_fn=Dataset_discrim.collate_fn)
 
-        net.train()
-        net.train_network(loader)
-        net.save_ckpt()
-
-        net.eval()
-        net.get_r_squared(testing_data)
+        if train:
+            net.train()
+            net.train_network(loader)
+            net.save_ckpt()
+            print('Testing discriminator...')
+            net.eval()
+            net.get_r_squared(testing_data)
 
         seed_vec, activity, smi_data = get_opt_input(self.discrim_data_dir, 'input_mols_filtered.csv',
                                                                     'input_train.csv', 'input_target.csv')
-        print('Seed smiles {} and activity {}'.format(smi_data, activity))
+        pred_act = ODO.predict_with_discrim(self, [seed_vec])
+        print('Seed smiles {} with true activity {} and predicted activity {}'.format(smi_data, activity, pred_act))
         if optimize:
             opt = Optimize(data, net, seed_vec, target=target_property)
             msd = np.average((opt.solution-seed_vec)**2)
+
+            neighbour = ODO.k_near_search(self, opt.solution, self.gen_data_dir+'vector_based/vecs.csv')
+            msd_lib = np.average((opt.solution - neighbour) ** 2)
+            print('MSD between gen lib and og solution = {}'.format(msd_lib))
+
+            opt.solution = (neighbour + opt.solution) / 2
+            msd_lib = np.average((opt.solution-neighbour)**2)
+            print('MSD between gen lib and fudged solution = {}'.format(msd_lib))
+
             print('MSD between seed and solution = {}'.format(msd))
+
+            if (neighbour == seed_vec).all():
+                print('Seed is neighbour')
+
             return opt.property, opt.solution
         else:
             return None
+
+    def k_near_search(self, vector, lib_vec_file, num_neighbours=1):
+        # vector is vector for compund we want to search for neigbours of
+        # lib_vec_file is a libary of compunds in vector form which will be search for neighbours
+        all_neigh_dist = []
+        all_neigh_index = []
+        all_neigh_vec = []
+        chunksize = 100000
+        print('Looking for nearest neighbour:')
+        for i, chunk in enumerate(pd.read_csv(lib_vec_file, chunksize=chunksize, header=0)):
+            print('Evaluating chunk {} of length {}'.format(i, len(chunk)))
+            # correct heading order
+            chunk = chunk.reindex(columns=get_headings())
+            traning_data = chunk.values
+            tree = spatial.KDTree(traning_data)
+            ans = tree.query(np.array(vector), k=num_neighbours)
+            all_neigh_dist.append(ans[0])
+            all_neigh_index.append(ans[1] + (i * chunksize))
+            all_neigh_vec.append(traning_data[ans[1]])
+
+        result = [x for _, x in sorted(zip(all_neigh_dist, all_neigh_vec), key=lambda pair: pair[0])]
+        return result[0]
 
     def discrim_to_gen(self):
         float_bool = get_float_bool(self.discrim_data_dir, 'float_bool.csv')
@@ -212,7 +266,7 @@ class ODO(object):
         else:
             Prior.rnn.load_state_dict(torch.load(ckpt_file, map_location=lambda storage, loc: storage))
 
-        all_smi = []
+        all_smi = set()
         valid = 0
         with open('./output_smi.csv', 'w') as file:
             for j, test_vec in enumerate(data):
@@ -222,8 +276,9 @@ class ODO(object):
                     smiles = seq_to_smiles(seqs, voc)[0]
                     if Chem.MolFromSmiles(smiles):
                         valid += 1
-                        all_smi.append(smiles)
+                        all_smi.add(smiles)
                         file.write(smiles + str(',{}\n'.format(j)))
+        all_smi = list(all_smi)
         print("\n{:>4.1f}% valid SMILES".format(100 * (valid / (samples * len(data)))))
         return all_smi
 
